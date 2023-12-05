34f9e8ee2766e082282da3898e8043986ae23a50
Jaydip Vidhate <jaydipvidhate@gmail.com>
2023-12-05 20:53:59 +0530

    Added Hero Section with responsive nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        //Dark
+        primary: "#008191",
+        black: "#000000",
+        white: "#ffffff",
+
+        //flag
+        orange: "#ff671f",
+        white: "#ffffff",
+        green: "#046a38",
+      },
+      colors: {
+        primary: "#008191",
+      },
+      borderColor: {
+        //Dark
+        primaryBorderDarkTheme: "#008191",
+      },
+      aspectRatio: {
+        banner: "16 / 9",
+        portrait: "12/16",
+        newsCardImage: "1.25 / 1",
+        largeCard: "21/9",
+      },
+      flex: {
+        flexProductSm: "0 0 60%",
+        flexProductMd: "0 0 40%",
+        flexCollectionsMd: "0 0 60%",
+        flexProductLg: "0 0 24%",
+      },
+      width: {
+        productGap: "48%",
+        productGapLg: "30%",
+        cartWidth: "94%",
+      },
+      maxWidth: {
+        lg: 1400,
+        md: 760,
+      },
+      keyframes: {
+        scroll: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
       },
     },
   },
   plugins: [],
-}
+};
